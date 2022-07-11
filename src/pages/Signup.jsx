175a2c39c7115b09f752c78a590fbc608bf3ab25
SyedSibtainRazvi <Syedsibtain191@gmail.com>
2022-07-11 20:03:55 +0530

    doc: signup added

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,8 +1,18 @@
 import React from 'react'
+import HeroBg from '../assets/bg-sign-up.jpg'
 
 const Signup = () => {
   return (
-    <div>Signup</div>
+    <>
+      <div className='w-full h-screen'>
+        <img className=' hidden sm:block w-full h-full object-cover absolute'
+          src={HeroBg}
+          alt="" />
+        <div className='absolute w-full h-full bg-black/50'>
+        </div>
+      </div>
+
+    </>
   )
 }
 
