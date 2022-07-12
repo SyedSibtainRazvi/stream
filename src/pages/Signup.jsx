f6cd4168bc8638d0870c4e585aa10dfeec7d4a97
SyedSibtainRazvi <Syedsibtain191@gmail.com>
2022-07-12 20:01:58 +0530

    style: bg added

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { UserAuth } from '../context/AuthContext';
+import bgHero from '../assets/bg-sign-up.jpg'
 
 const Signup = () => {
   const [email, setEmail] = useState('');
@@ -23,7 +24,7 @@ const Signup = () => {
       <div className='w-full h-screen'>
         <img
           className='hidden sm:block absolute w-full h-full object-cover'
-          src='https://assets.nflxext.com/ffe/siteui/vlv3/f841d4c7-10e1-40af-bcae-07a3f8dc141a/f6d7434e-d6de-4185-a6d4-c77a2d08737b/US-en-20220502-popsignuptwoweeks-perspective_alpha_website_medium.jpg'
+          src= {bgHero}
           alt='/'
         />
         <div className='bg-black/60 fixed top-0 left-0 w-full h-screen'></div>
@@ -37,17 +38,21 @@ const Signup = () => {
               >
                 <input
                   onChange={(e) => setEmail(e.target.value)}
-                  className='p-3 my-2 bg-gray-700 rouded'
+                  className='p-3 my-2 bg-gray-700 rounded'
                   type='email'
                   placeholder='Email'
                   autoComplete='email'
+                  required
+
                 />
                 <input
                   onChange={(e) => setPassword(e.target.value)}
-                  className='p-3 my-2 bg-gray-700 rouded'
+                  className='p-3 my-2 bg-gray-700 rounded'
                   type='password'
                   placeholder='Password'
                   autoComplete='current-password'
+                  required
+
                 />
                 <button className='bg-red-600 py-3 my-6 rounded font-bold'>
                   Sign Up
